4ef3eefbdeefdc44cad215919d27521fbd2c50cd
Feynman Liang <feynman.liang@gmail.com>
2015-12-12 17:32:15 +0000

    Adds loop over features and saves train/cv

--- single-hidden-layer/loop-context-widths.zsh
+++ single-hidden-layer/loop-context-widths.zsh
@@ -1,12 +1,29 @@
 #!/usr/bin/zsh
 
-modelDir=$(dirname `pwd`)/MFC_E_D_A_Z_FlatStart/
+features=(
+"FBK"
+"MFC"
+)
+alignDirs=(
+$(dirname `pwd`)/"FBK_D_A_Z_FlatStart"
+$(dirname `pwd`)/"MFC_E_D_A_Z"
+)
+envDirs=(
+"../../convert/fbk25d/env/environment_D_A_Z"
+"../../convert/mfc13d/env/environment_E_D_A_Z"
+)
 
-contexts=(
-  #"0"
-  #"-1,0,1"
-  #"-2,-1,0,1,2"
-  #"-3-2,-1,0,1,2,3"
+integer i
+for i in {1..$#features}; do
+  feature=$features[i]
+  alignDir=$alignDirs[i]
+  envDir=$envDirs[i]
+
+  contexts=(
+  "0"
+  "-1,0,1"
+  "-2,-1,0,1,2"
+  "-3-2,-1,0,1,2,3"
   "-4,-3,-2,-1,0,1,2,3,4"
   "-5,-4,-3,-2,-1,0,1,2,3,4,5"
   "-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6"
@@ -14,37 +31,44 @@ contexts=(
   "-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8"
   "-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9"
   "-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10"
-)
+  )
 
-for context in $contexts; do
-  print "Training context=$context"
-  # remove trained dnn dir if present
-  if [[ -d ./MH0/dnntrain ]]; then
-    rm -rf ./MH0/dnntrain
-  fi
+  for context in $contexts; do
+    print "Training feature=$feature,context=$context"
 
-  split=("${(@s/,/)context}") # @ modifier splits string into array
-  contextLength=${#split}
-  inputLayerSize=$(($contextLength*39))
+    #remove trained dnn dir if present
+    if [[ -d ./MH0/dnntrain ]]; then
+      rm -rf ./MH0/dnntrain
+    fi
 
-  print $context
-  print $split
-  print $contextLength
-  print $inputLayerSize
+    split=("${(@s/,/)context}") # @ modifier splits string into array
+    contextLength=${#split}
+    inputLayerSize=$(($contextLength*39))
 
-  # set the context length and input layer size
-  sed -i "s/set CONTEXTSHIFT=[^ ]*/set CONTEXTSHIFT=$context/" ./HTE.dnntrain
-  sed -i "s/set DNNSTRUCTURE=[^X]*/set DNNSTRUCTURE=${inputLayerSize}/" ./HTE.dnntrain
+    #set the context length and input layer size
+    sed -i "s/set CONTEXTSHIFT=[^ ]*/set CONTEXTSHIFT=$context/" ./HTE.dnntrain
+    sed -i "s/set DNNSTRUCTURE=[^X]*/set DNNSTRUCTURE=${inputLayerSize}/" ./HTE.dnntrain
 
-  # train the DNN
-  ../../tools/steps/step-dnntrain \
-    -DNNTRAINHTE `pwd`/HTE.dnntrain -USEGPUID 0\
-    ../../convert/mfc13d/env/environment_E_D_A_Z \
-    $modelDir/align-mono-hmm84/align/timit_train.mlf $modelDir/mono/hmm84/MMF \
-    $modelDir/mono/hmms.mlist MH0/dnntrain
+    #train the DNN
+    ../../tools/steps/step-dnntrain \
+      -DNNTRAINHTE `pwd`/HTE.dnntrain -USEGPUID 0\
+      $envDir \
+      $alignDir/align-mono-hmm84/align/timit_train.mlf $alignDir/mono/hmm84/MMF \
+      $alignDir/mono/hmms.mlist MH0/dnntrain
 
-  # test the DNN
-  ../../tools/steps/step-decode \
-    `pwd`/MH0/dnntrain dnn3.finetune MH0/decode-dnn3.finetune-context=$context
+    #copy fine-tuning log for train/cv frame classification performance
+    cp ./MH0/dnntrain/dnn3.finetune/LOG \
+      ./MH0/dnn3.finetune-features=${feature}-context=${context}
+
+    #test the DNN
+    for insword in -32.0 -16.0 -8.0 -4.0 -2.0 0.0 2.0 4.0; do
+      print "Decoding INSWORD=${insword}"
+      ../../tools/steps/step-decode \
+        -INSWORD $insword \
+        `pwd`/MH0/dnntrain dnn3.finetune \
+        MH0/decode-dnn3.finetune-features=${feature}-context=${context}-insword=${insword}
+    done
+  done
 done
+
 
