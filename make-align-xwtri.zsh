cd0b1ea7134ae6d21438abeb4d65625c09f5b3ce
Feynman Liang <feynman.liang@gmail.com>
2015-12-13 12:01:24 +0000

    Adds xwtri alignment

---
+++ make-align-xwtri.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+
+feature="MFC_E_D_A_Z"
+alignDir="${feature}_FlatStart"
+envDir="../convert/mfc13d/env/environment_E_D_A_Z"
+
+# train xwtri from MFC_E_D_A_Z_FlatStart monophones
+../tools/steps/step-xwtri \
+  -NUMMIXES 8 -ROVAL 200 -TBVAL 800 \
+  `pwd`/$alignDir/mono hmm14 $alignDir/xwtri
+
+# align frames
+../tools/steps/step-align $alignDir/xwtri hmm84 \
+  $alignDir/align-xwtri-hmm84
