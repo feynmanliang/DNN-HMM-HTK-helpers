712eeda7625938f033388bf7db288f40a1136781
Feynman Liang <feynman.liang@gmail.com>
2016-01-08 10:55:34 +0000

    Updates bigram language model params

--- bigram/bigram-train-decode.zsh
+++ bigram/bigram-train-decode.zsh
@@ -1,16 +1,17 @@
 #!/usr/bin/zsh
 
-dataDir=$(dirname `pwd`)/MFC_E_D_A_Z_FlatStart/
+envDir="../../convert/mfc13d/env/environment_E_D_A_Z"
+alignDir=$(dirname `pwd`)/MFC_E_D_A_Z_FlatStart/
 
 # generate pronunciation dictionary from word list
 python gen_dict_from_word_list.py word_list.txt lattice-phones.dct
 
-# train default (context-independent 5 hidden layer [-4,4] context window) DNN-HMM
+# train (MFCC, 6 context window, 5 layers, PTWD=1e-1, FTWD=1e-5, mono targets) DNN-HMM
 ../../tools/steps/step-dnntrain \
-  -USEGPUID 0 \
-  ../../convert/mfc13d/env/environment_E_D_A_Z \
-  $dataDir/align-mono-hmm84/align/timit_train.mlf $dataDir/mono/hmm84/MMF \
-  $dataDir/mono/hmms.mlist MH0/dnntrain
+  -DNNTRAINHTE `pwd`/HTE.dnntrain -USEGPUID 0 \
+  $envDir \
+  $alignDir/align-mono-hmm84/align/timit_train.mlf $alignDir/mono/hmm84/MMF \
+  $alignDir/mono/hmms.mlist MH0/dnntrain
 
 # decode using language model optimizing, insertion penalty and scale factor
 for ins in -32.0 -16.0 -8.0 -4.0 -2.0 0.0; do
