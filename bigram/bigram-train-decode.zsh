9439cb6c09a96dd073b077daa0922464b048f8cc
Feynman Liang <feynman.liang@gmail.com>
2016-01-08 12:08:25 +0000

    Fixes bigram LM decoding and adds print results

--- bigram/bigram-train-decode.zsh
+++ bigram/bigram-train-decode.zsh
@@ -1,7 +1,10 @@
 #!/usr/bin/zsh
+zmodload zsh/parameter
 
 envDir="../../convert/mfc13d/env/environment_E_D_A_Z"
 alignDir=$(dirname `pwd`)/MFC_E_D_A_Z_FlatStart/
+
+sleepsecs=30 # seconds to sleep between checking if decoding forks are done
 
 # generate pronunciation dictionary from word list
 python gen_dict_from_word_list.py word_list.txt lattice-phones.dct
@@ -16,10 +19,27 @@ python gen_dict_from_word_list.py word_list.txt lattice-phones.dct
 # decode using language model optimizing, insertion penalty and scale factor
 for ins in -32.0 -16.0 -8.0 -4.0 -2.0 0.0; do
   for sf in 1.0 2.0 4.0 8.0 16.0 32.0 64.0; do
+    print "Decoding INS=$ins, SF=$sf on train"
     ../../tools/steps/step-decode \
       -DECODEHTE `pwd`/HTE.bigram-lm \
+      -INSWORD $ins -GRAMMARSCALE $sf\
+      -SUBTRAIN \
       `pwd`/MH0/dnntrain \
       dnn7.finetune \
-      MH0/decode-dnn7.finetune-ins-${ins}-sf-${sf}
+      MH0/decode-dnn7.finetune-trainSub-ins-${ins}-sf-${sf} &
+
+    print "Decoding INS=$ins, SF=$sf on test"
+    ../../tools/steps/step-decode \
+      -DECODEHTE `pwd`/HTE.bigram-lm \
+      -INSWORD $ins -GRAMMARSCALE $sf\
+      `pwd`/MH0/dnntrain \
+      dnn7.finetune \
+      MH0/decode-dnn7.finetune-ins-${ins}-sf-${sf} &
   done
 done
+while (( ${#jobstates} )); do
+  print "Current active jobs: ${#jobstates}"
+  print "Sleeping for $sleepsecs seconds"
+  sleep ${sleepsecs}
+done
+print "Done all decoding jobs"
---
+++ bigram/print-results.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/zsh
+
+# final epoch train/cv accuracy
+trainAcc=$(cat \
+  ./MH0/dnntrain/*.finetune/LOG
+  | tail -n17 \
+  | head -n5 \
+  | grep -P ".*Accuracy.*" \
+  | awk -F '[ =]' '{print $5}')
+cvAcc=$(cat \
+  ./MH0/dnntrain/*.finetune/LOG
+  | tail -n11 \
+  | grep -P ".*Accuracy.*" \
+  | awk -F '[ =]' '{print $5}')
+print $trainAcc\;$cvAcc
+
+# decode accuracies
+for ins in -32.0 -16.0 -8.0 -4.0 -2.0 0.0; do
+  for sf in 1.0 2.0 4.0 8.0 16.0 32.0 64.0; do
+    decodeAcc=$(cat \
+      ./MH0/d
+      MH0/decode-dnn*.finetune-trainSub-ins-${ins}-sf-${sf}/test/LOG \
+      | tail -n5 \
+      | head -n1 \
+      | awk -F '[ =]' '{print $5}')
+    print $ins\;$sf\;Train.Set\;$decodeAcc
+
+    decodeAcc=$(cat \
+      MH0/decode-dnn*.finetune-ins-${ins}-sf-${sf}/test/LOG \
+      | tail -n5 \
+      | head -n1 \
+      | awk -F '[ =]' '{print $5}')
+    print $ins\;$sf\;Test.Set\;$decodeAcc
+  done
+done
