49bf100128b1510ac13e51c1ae52c86f1ffe97bf
Feynman Liang <feynman.liang@gmail.com>
2016-01-08 14:40:26 +0000

    Adds bigram results

---
+++ bigram/bigram-qsub.sh
@@ -0,0 +1,2 @@
+cd /home/fl350/MLSALT2/practical2/exp/bigram/
+zsh bigram-train-decode.zsh
--- bigram/print-results.zsh
+++ bigram/print-results.zsh
@@ -2,13 +2,13 @@
 
 # final epoch train/cv accuracy
 trainAcc=$(cat \
-  ./MH0/dnntrain/*.finetune/LOG
+  ./MH0/dnntrain/*.finetune/LOG \
   | tail -n17 \
   | head -n5 \
   | grep -P ".*Accuracy.*" \
   | awk -F '[ =]' '{print $5}')
 cvAcc=$(cat \
-  ./MH0/dnntrain/*.finetune/LOG
+  ./MH0/dnntrain/*.finetune/LOG \
   | tail -n11 \
   | grep -P ".*Accuracy.*" \
   | awk -F '[ =]' '{print $5}')
@@ -18,7 +18,6 @@ print $trainAcc\;$cvAcc
 for ins in -32.0 -16.0 -8.0 -4.0 -2.0 0.0; do
   for sf in 1.0 2.0 4.0 8.0 16.0 32.0 64.0; do
     decodeAcc=$(cat \
-      ./MH0/d
       MH0/decode-dnn*.finetune-trainSub-ins-${ins}-sf-${sf}/test/LOG \
       | tail -n5 \
       | head -n1 \
