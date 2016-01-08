96dd46e0238ec77aab033930de4d5ebb08a6223f
Feynman Liang <feynman.liang@gmail.com>
2016-01-08 12:07:58 +0000

    Adds print result and decode to realignment

--- realignment/retrain.zsh
+++ realignment/retrain.zsh
@@ -2,11 +2,11 @@
 
 envDir="../../convert/mfc13d/env/environment_E_D_A_Z"
 alignDir=$(dirname `pwd`)/MFC_E_D_A_Z_FlatStart/
-realignDir=`pwd`/MH0/align-dnn7-train
+realignDir=`pwd`/MH0/align-dnn7.finetune-train
 
 # re-train the DNN
 ../../tools/steps/step-dnntrain \
   -DNNTRAINHTE $(dirname `pwd`)/bigram/HTE.dnntrain -USEGPUID 0 \
   $envDir \
   $realignDir/align/timit_train.mlf $alignDir/mono/hmm84/MMF \
-  $alignDir/mono/hmms.mlist MH0/dnntrain-realign
+  $alignDir/mono/hmms.mlist MH0/dnntrain
