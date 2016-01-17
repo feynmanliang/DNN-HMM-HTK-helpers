c2691331874c486b261d4d9907ec39c2af0be5be
Feynman Liang <feynman.liang@gmail.com>
2016-01-17 11:28:34 +0000

    Updates realignment code

--- realignment/retrain.zsh
+++ realignment/retrain.zsh
@@ -1,12 +1,14 @@
 #!/usr/bin/zsh
 
+
 envDir="../../convert/mfc13d/env/environment_E_D_A_Z"
-alignDir=$(dirname `pwd`)/MFC_E_D_A_Z_FlatStart/
+mfcDir=$(dirname `pwd`)/"MFC_E_D_A_Z_FlatStart"
+hmmDir=$(dirname `pwd`)/"MFC_E_D_A_Z_FlatStart"
 realignDir=`pwd`/MH0/align-dnn7.finetune-train
 
 # re-train the DNN
 ../../tools/steps/step-dnntrain \
   -DNNTRAINHTE $(dirname `pwd`)/bigram/HTE.dnntrain -USEGPUID 0 \
   $envDir \
-  $realignDir/align/timit_train.mlf $alignDir/mono/hmm84/MMF \
-  $alignDir/mono/hmms.mlist MH0/dnntrain
+  $realignDir/align/timit_train.mlf $mfcDir/mono/hmm84/MMF \
+  $hmmDir/mono/hmms.mlist MH0/dnntrain
