c8ac245bf6ef7bead5ee1f9b50e2054e84ed5b62
Feynman Liang <feynman.liang@gmail.com>
2016-01-24 16:27:56 +0000

    Updates

--- single-realignment/1-realign.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-# realign using trained DNN
-# uses DNN for bigram LM, which should be the best so far
-../../tools/steps/step-align \
-  $(dirname `pwd`)/bigram/MH0/dnntrain \
-  dnn7.finetune \
-  MH0/align-dnn7.finetune-train
--- single-realignment/decode-realign-qsub.sh
+++
@@ -1,2 +0,0 @@
-cd /home/fl350/MLSALT2/practical2/exp/realignment/
-zsh decode.zsh
